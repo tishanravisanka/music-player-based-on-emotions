1b2e1b21ac65ec5a0bd02324d2c9145c25c7d102
Tishan Ravisanka <ktrwickramasinghe@gmail.com>
2021-05-16 15:08:57 +0530

    Update dp.py

--- dp.py
+++ dp.py
@@ -1,5 +1,5 @@
 from deepface import DeepFace
-obj = DeepFace.analyze(img_path = "C:/Users/ACER/Desktop/emotion detection/new/img.jpg", actions = ['age', 'gender', 'emotion'])
+obj = DeepFace.analyze(img_path = "C:/Users/ACER/Desktop/emotion detection/new/img.jpg", actions = ['emotion'])
 #objs = DeepFace.analyze(["img1.jpg", "img2.jpg", "img3.jpg"]) #analyzing multiple faces same time
-print(obj["age"]," years old "," ",obj["dominant_emotion"]," ", obj["gender"])
+print(obj["dominant_emotion"])
 # input();
