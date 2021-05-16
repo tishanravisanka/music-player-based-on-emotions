b432c609867c8f80fd9d48b9f2b2dd089139368c
Tishan Ravisanka <ktrwickramasinghe@gmail.com>
2021-05-16 20:15:21 +0530

    Delete dp.py

--- dp.py
+++
@@ -1,5 +0,0 @@
-from deepface import DeepFace
-obj = DeepFace.analyze(img_path = "C:/Users/ACER/Desktop/emotion detection/new/img.jpg", actions = ['emotion'])
-#objs = DeepFace.analyze(["img1.jpg", "img2.jpg", "img3.jpg"]) #analyzing multiple faces same time
-print(obj["dominant_emotion"])
-# input();
